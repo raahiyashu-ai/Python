7646d5c33810ec0c2e2f649fb4294d63a7374c7e
raahiyashu-ai <raahiyashu@gmail.com>
2026-04-10 21:08:06 +0530

    Add files via upload

---
+++ alpha.py
@@ -0,0 +1,6 @@
+text = int(input("Enter your  text you want:"))
+
+if text.isalpha():
+    print("The string contains only alphabets")
+else:
+    print("The string does not contain only alphabets")
