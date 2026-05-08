318d9f2ce6872370bea5a4469d97e88a4ec08a3f
raahiyashu-ai <raahiyashu@gmail.com>
2026-05-08 21:31:53 +0530

    Add files via upload

---
+++ 3 modes .py
@@ -0,0 +1,6 @@
+a= input ("Entyer a word :")
+for i in  a :
+    if (i=='a'):
+        print("A is found")
+else:
+    print("A is not found")
---
+++ 3 modes3.py
@@ -0,0 +1,7 @@
+var = int(input("Enter you number var:"))
+while var >0:
+    var= var-1
+    if var==5:
+        continue
+    print('\nCurrent varible value:',var)
+    print("\nGood byee!")
