bb23f960def12b8e1952ccceaff0e08d0484780f
raahiyashu-ai <raahiyashu@gmail.com>
2026-05-27 20:53:13 +0530

    Add files via upload

---
+++ tuplex.py
@@ -0,0 +1,14 @@
+truplex=("truple",False,3.2,1)
+print(truplex)
+truplex =(4,6,2,8,3,1)
+print(truplex)
+truplex=truplex +(9,)
+print(truplex)
+truple1=(50,10,60,70,50)
+print(truple1.count(50))
+truplex =(2,4,5,4,6,7,8,6,1)
+_slice=truplex[3:5]
+print(_slice)
+print(_slice)
+_slice=truplex[:6]
+print(_slice)
